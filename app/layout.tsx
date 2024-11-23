c2537956cd58e06733a0986838fd1012aa98af81
Rak-Ana <Rak-Ana>
2024-11-23 13:33:19 +0700

    initail commit

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Sarabun } from "next/font/google"
+import "./globals.css";
+// import Head from "@/components/head";
+// import Foot from "@/components/foot";
+
+const sarabun = Sarabun({
+  weight: '400',
+  subsets: ['thai']
+})
+
+export const metadata: Metadata = {
+  title: "Next Basic",
+  description: "This is my sample application",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${sarabun.className}`}
+      >
+        
+        <main>
+          {children}
+        </main>
+        
+      </body>
+    </html>
+  );
+}
